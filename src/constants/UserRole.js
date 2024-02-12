e4f5bdacdb267df261d62841cd05509315debce9
Avro29 <ravonirinaf290303@gmail.com>
2024-02-12 20:10:23 +0300

    Create UserRole.js
    IMP user role

---
+++ src/constants/UserRole.js
@@ -0,0 +1,7 @@
+class UserRole {
+    static ROLE_USER_CLIENT = 'ROLE_USER_CLIENT';
+    static ROLE_USER_MANAGER = 'ROLE_USER_MANAGER';
+    static ROLE_USER_FINANCE = 'ROLE_USER_EMPLOYÉ';
+}
+
+module.exports = UserRole;
