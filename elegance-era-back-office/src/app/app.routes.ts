5684a0f6b10e5961875da58daa70ab245ca3132b
TsilavinaRakotomavo <89471900+TsilavinaRakotomavo@users.noreply.github.com>
2024-02-16 22:04:16 +0300

    creating routes

--- elegance-era-back-office/src/app/app.routes.ts
+++ elegance-era-back-office/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { StatistiquesComponent } from './components/statistiques/statistiques.component';
+import { PersonnelsComponent } from './components/personnels/personnels.component';
+import { ServicesComponent } from './components/services/services.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'statistiques',
+    pathMatch: 'full',
+  },
+  {
+    path: 'statistiques',
+    component: StatistiquesComponent,
+  },
+  {
+    path: 'personnels',
+    component: PersonnelsComponent,
+  },
+  {
+    path: 'services',
+    component: ServicesComponent,
+  },
+];
