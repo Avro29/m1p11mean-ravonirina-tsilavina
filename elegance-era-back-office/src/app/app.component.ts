96a447989654aa7f08e492d342470f51b68e612a
TsilavinaRakotomavo <89471900+TsilavinaRakotomavo@users.noreply.github.com>
2024-02-16 22:04:01 +0300

    minimize app.component.ts using components

--- elegance-era-back-office/src/app/app.component.ts
+++ elegance-era-back-office/src/app/app.component.ts
@@ -1,59 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-declare interface RouteInfo {
-  path: string;
-  title: string;
-  icon: string;
-  class: string;
-}
-
-export const ROUTES: RouteInfo[] = [
-  {
-    path: '/statistiques',
-    title: 'Statistiques',
-    icon: 'business_chart-bar-32',
-    class: '',
-  },
-  {
-    path: '/personnels',
-    title: 'Gestion du personnel',
-    icon: 'users_single-02',
-    class: '',
-  },
-  {
-    path: '/services',
-    title: 'Gestion des services',
-    icon: 'shopping_bag-16',
-    class: '',
-  },
-];
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { StatistiquesComponent } from './components/statistiques/statistiques.component';
+import { PersonnelsComponent } from './components/personnels/personnels.component';
+import { ServicesComponent } from './components/services/services.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, RouterModule, NgbModule],
+  imports: [
+    RouterOutlet,
+    NgbModule,
+    FooterComponent,
+    SidebarComponent,
+    NavbarComponent,
+    StatistiquesComponent,
+    PersonnelsComponent,
+    ServicesComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  title = 'elegance-era-back-office';
-  menuItems: any[] = [];
-  test: Date = new Date();
-
   constructor() {}
 
-  ngOnInit(): void {
-    this.menuItems = ROUTES.filter((menuItem) => menuItem);
-  }
-
-  isMobileMenu() {
-    if (window.innerWidth > 991) {
-      return false;
-    }
-    return true;
-  }
+  ngOnInit(): void {}
 }
