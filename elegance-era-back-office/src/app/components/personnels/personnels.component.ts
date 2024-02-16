02c44852df0ed50e7a1915f0e24967b2772a3436
TsilavinaRakotomavo <89471900+TsilavinaRakotomavo@users.noreply.github.com>
2024-02-16 22:02:46 +0300

    create personnels component

---
+++ elegance-era-back-office/src/app/components/personnels/personnels.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-personnels',
+  templateUrl: './personnels.component.html',
+  styleUrls: ['./personnels.component.css'],
+})
+export class PersonnelsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
