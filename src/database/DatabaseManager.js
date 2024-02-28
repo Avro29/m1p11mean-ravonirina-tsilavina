47b4e42b1c603ba0cc5778428e0ba38d3ff3793f
Ravonirina <ravonirinaf290303@gmail.com>
2024-02-28 11:39:22 +0300

    Debug

--- src/database/DatabaseManager.js
+++ src/database/DatabaseManager.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const uri = "mongodb://127.0.0.1/27017/Salon";
 const dbURI = process.env.DB_URI;
+console.log('dbURI:', process.env.DB_URI)
 mongoose.set("strictQuery", false);
 mongoose
 	.connect(dbURI)
